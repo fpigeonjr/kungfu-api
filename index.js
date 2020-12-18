312452bd41655351a36231226e31fa54ae6b38cb
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-12-18 09:40:48 -0600

    adds playground

--- index.js
+++ index.js
@@ -66,7 +66,12 @@ const resolvers = {
   },
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+})
 
 server
   .listen({
